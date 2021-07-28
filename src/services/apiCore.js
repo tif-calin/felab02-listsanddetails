3b44b05686b53f4d069b15a4cf8d53fad0367fa3
culi <culitif@tuta.io>
2021-07-28 11:02:00 -0700

    fix CORE api call issues

--- src/services/apiCore.js
+++ src/services/apiCore.js
@@ -1,15 +1,44 @@
 
-const fetchCorePapers = async (days = 7) => {
+const fetchCoreValidPapers = async (num = 30) => {
+  const date = new Date();
+  const papers = [];
+
+  // loop through calls until we get the intended amount of papers
+  let i = 1;
+  while (papers.length < num) {
+    // get papers from core api
+    const corePapers = await fetchCorePapers(7, i);
+    
+    // loop through papers and add valid ones to array
+    corePapers.forEach(paper => {
+      if (paper.publisher 
+        && Number(paper.year) <= Number(date.getFullYear())
+        && paper.doi && paper.doi.length > 0
+      ) papers.push(paper);
+
+      // if we have enough papers, return array
+      if (papers.length >= num) return papers;
+    });
+
+    i++;
+  }
+
+  return papers;
+};
+
+const fetchCorePapers = async (days = 7, page = 1) => {
   // create url with params
   const date = new Date(new Date().getTime() - (days * 24 * 60 * 60 * 1000));
   const url = process.env.CORE_API_URL
-    + '/articles/search/repositoryDocument.metadataUpdated:>'
+    + '/articles/search/"repositoryDocument.metadataUpdated:>'
     + date.toISOString().split('T')[0]
-    + '?'
+    + ' AND year:<'
+    + (Number(date.getFullYear()) + 1)
+    + '"?'
     + new URLSearchParams({ 
       metaData: true,
-      page: 1,
-      pageSize: 100,
+      page,
+      pageSize: 200,
       apiKey: process.env.CORE_API_KEY
     })
   ;
@@ -23,14 +52,14 @@ const fetchCorePapers = async (days = 7) => {
 
 const fetchCorePaper = async coreId => {
   // create url with params
-  const url = process.env.CORE_API_KEY + '/articles/get/' 
+  const url = process.env.CORE_API_URL + '/articles/get/' 
     + coreId
     + '?'
     + new URLSearchParams({
-      apiKey: process.env.CORE_API_KEY,
       fullText: true,
       similar: true,
-      urls: true
+      urls: true,
+      apiKey: process.env.CORE_API_KEY
     })
   ;
 
@@ -38,7 +67,7 @@ const fetchCorePaper = async coreId => {
   const resp = await fetch(url);
   const json = await resp.json();
 
-  return json.error ? null : json.results;
+  return json.error ? null : json.data;
 };
 
-export { fetchCorePapers, fetchCorePaper };
+export { fetchCorePapers, fetchCorePaper, fetchCoreValidPapers };
