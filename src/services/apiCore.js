754da8a9052dd0c7d0d12f613ae4407f0ce043c5
culi <culitif@tuta.io>
2021-07-27 20:58:43 -0700

    fix CORE api service functions

--- src/services/apiCore.js
+++ src/services/apiCore.js
@@ -1,40 +1,44 @@
+
 const fetchCorePapers = async (days = 7) => {
-  // create proper url
+  // create url with params
   const date = new Date(new Date().getTime() - (days * 24 * 60 * 60 * 1000));
   const url = process.env.CORE_API_URL
-    + '/articles/search/repositoryDocument.metadataUpdated%3A%3E'
-    + (`${date.getFullYear()}-${(date).getDate() - days}-${date.getMonth()}`
-      .replace('/', '-')
-    )
+    + '/articles/search/repositoryDocument.metadataUpdated:>'
+    + date.toISOString().split('T')[0]
+    + '?'
+    + new URLSearchParams({ 
+      metaData: true,
+      page: 1,
+      pageSize: 100,
+      apiKey: process.env.CORE_API_KEY
+    })
   ;
   
   // get json response
-  const resp = await fetch(url).query({ 
-    apiKey: process.env.CORE_API_KEY,
-    page: 1,
-    pageSize: 100
-  });
+  const resp = await fetch(url);
   const json = await resp.json();
 
-  console.log(json);
-  return json.results.data;
+  return json.error ? [] : json.data;
 };
 
 const fetchCorePaper = async coreId => {
-  // create url
-  const url = process.env.CORE_API_KEY + '/articles/get/' + coreId;
+  // create url with params
+  const url = process.env.CORE_API_KEY + '/articles/get/' 
+    + coreId
+    + '?'
+    + new URLSearchParams({
+      apiKey: process.env.CORE_API_KEY,
+      fullText: true,
+      similar: true,
+      urls: true
+    })
+  ;
 
   // get json response
-  const resp = await fetch(url).query({
-    apiKey: process.env.CORE_API_KEY,
-    fullText: true,
-    similar: true,
-    urls: true
-  });
+  const resp = await fetch(url);
   const json = await resp.json();
 
-  console.log(json);
-  return json.results;
+  return json.error ? null : json.results;
 };
 
 export { fetchCorePapers, fetchCorePaper };
