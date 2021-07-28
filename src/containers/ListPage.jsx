e1b6b7468c3ff93815239912a34bf70a53b402e7
culi <culitif@tuta.io>
2021-07-28 11:02:35 -0700

    use localstorage and only fetch validated papers

--- src/containers/ListPage.jsx
+++ src/containers/ListPage.jsx
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { fetchCorePapers } from '../services/apiCore.js';
+import { fetchCoreValidPapers } from '../services/apiCore.js';
 import List from '../components/list/List.jsx';
 
 const ListPage = props => {
   // set up state
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // component did mount
-  useEffect(async () => { 
-    setItems(await fetchCorePapers());
+  useEffect(async () => {
+    setItems(JSON.parse(window.localStorage.getItem('items')) || []);
+    fetchCoreValidPapers()
+      .then(papers => setItems(papers))
+      .then(() => setLoading(false))
+    ;
 
     return () => setItems([]);
   }, []);
+
+  // set localStorage
+  useEffect(() => {
+    window.localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
 
   // render page
   return (
     <div className="ListPage">
       {Boolean(items.length) &&
-        <List items={items} {...props} />
+        <List items={items} loading={loading} {...props} />
       }
     </div>
   );
